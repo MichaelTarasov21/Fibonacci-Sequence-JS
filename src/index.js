0a3a26cd21e0f93b9f2bd223654908f44590d153
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-03-06 12:10:04 -0500

    Finished Fibonacci Calculator
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+function run() {
+  const input = document.getElementById("Input").value;
+  const term = parseInt(input);
+  if (input != parseInt(input)) {
+    alert("Please enter a whole number");
+    return;
+  }
+  if (term >= 0) {
+    calculate(term);
+  } else {
+    alert("Please enter a positive number");
+    return;
+  }
+}
+function calculate(term) {
+    let sequence = [0,1]
+    while (term > sequence.length){
+        const nextterm = sequence[sequence.length - 1] + sequence[sequence.length - 2]
+        sequence.push(nextterm)
+    }
+    document.getElementById("answer").innerHTML = `Term ${term} in the Fibonacci Sequence is ${sequence[term - 1]}`
+}
+document.getElementById("submit").addEventListener("click", run);
